1171b65e23457b072bcbad3894424cbfeceaa974
mateussk83 <mateussk83@gmail.com>
2023-11-12 16:08:03 -0300

    feat: create maven project

---
+++ src/test/java/br/com/aps8/relatorio_api/RelatorioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aps8.relatorio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatorioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
